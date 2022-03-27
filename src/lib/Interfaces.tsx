b4e8213471d4c0d4b5b0c23c980a377507bcb53d
IllusiveBagel <hawkentroop@gmail.com>
2022-03-27 22:22:53 +0100

    Converted vector.js to Typescript

--- src/lib/Interfaces.tsx
+++ src/lib/Interfaces.tsx
@@ -1,8 +1,72 @@
+import { Architect } from "synaptic";
 import { Creature } from "./creature";
 
+// Interfaces
 export interface IWorld {
     creatures: Creature[];
     width: number;
     height: number;
     context: CanvasRenderingContext2D | null;
 }
+
+export interface ICreature {
+    network: Architect.Perceptron;
+    world: IWorld;
+    mass: number;
+    maxspeed: number;
+    maxforce: number;
+    lookRange: number;
+    length: number;
+    base: number;
+    HALF_PI: number;
+    TWO_PI: number;
+    location: IVector;
+    velocity: IVector;
+    acceleration: IVector;
+    color: string;
+}
+
+export interface IVector {
+    x: number;
+    y: number;
+}
+
+export interface IVectorInterface {
+    x: number;
+    y: number;
+    set: CoordinateVectorFunction;
+    add: VectorVectorFunction;
+    sub: VectorVectorFunction;
+    mul: NumberVectorFunction;
+    div: NumberVectorFunction;
+    mag: NumberFunction;
+    normalize: VectorFunction;
+    angle: NumberFunction;
+    setMag: NumberVectorFunction;
+    setAngle: NumberVectorFunction;
+    rotate: NumberVectorFunction;
+    limit: NumberVectorFunction;
+    angleBetween: VectorNumberFunction;
+    dot: VectorNumberFunction;
+    lerp: VectorNumberVectorFunction;
+    dist: VectorNumberFunction;
+    copy: AnyFunction;
+    random: VectorFunction;
+}
+
+// Types
+type CoordinateVectorFunction = (x: number, y: number) => IVectorInterface;
+
+type VectorVectorFunction = (v: IVectorInterface) => IVectorInterface;
+
+type NumberVectorFunction = (s: number) => IVectorInterface;
+
+type NumberFunction = () => number;
+
+type VectorFunction = () => IVectorInterface;
+
+type VectorNumberFunction = (v: IVectorInterface) => number;
+
+type VectorNumberVectorFunction = (v: IVectorInterface, amt: number) => IVectorInterface;
+
+type AnyFunction = () => any;
--- src/lib/Vector.tsx
+++ src/lib/Vector.tsx
@@ -1,113 +1,124 @@
-export function Vector(x, y)
-{
-	this.x = x;
-	this.y = y;
-}
+import { IVectorInterface } from "./Interfaces";
 
-Vector.prototype = {
-	set: function(x, y)
-	{
+export class Vector implements IVectorInterface {
+	x: number;
+	y: number;
+
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	set(x: number, y: number) {
 		this.x = x;
 		this.y = y;
 
 		return this;
-	},
-	add: function(v)
-	{
+	}
+
+	add(v: IVectorInterface) {
 		this.x += v.x;
 		this.y += v.y;
 
 		return this;
-	},
-	sub: function(v)
-	{
+	}
+
+	sub(v: IVectorInterface) {
 		this.x -= v.x;
 		this.y -= v.y;
 
 		return this;
-	},
-	mul: function(s)
-	{
+	}
+
+	mul(s: number) {
 		this.x *= s;
 		this.y *= s;
 
 		return this;
-	},
-	div: function(s)
-	{
-		!s && console.log("Division by zero!");
+	}
 
+	div(s: number) {
 		this.x /= s;
 		this.y /= s;
 
 		return this;
-	},
-	mag: function(){
+	}
+
+	mag() {
 		return Math.sqrt(this.x * this.x + this.y * this.y);
-	},
-	normalize: function()
-	{
+	}
+
+	normalize() {
 		var mag = this.mag();
 		mag && this.div(mag);
+
 		return this;
-	},
-	angle: function()
-	{
+	}
+
+	angle() {
 		return Math.atan2(this.y, this.x);
-	},
-	setMag: function(m)
-	{
+	}
+
+	setMag(m: number) {
 		var angle = this.angle();
 		this.x = m * Math.cos(angle);
 		this.y = m * Math.sin(angle);
+
 		return this;
-	},
-	setAngle: function(a)
-	{
+	}
+
+	setAngle(a: number) {
 		var mag = this.mag();
 		this.x = mag * Math.cos(a);
 		this.y = mag * Math.sin(a);
-		return this;
-	},
-	rotate: function(a)
-	{
-		this.setAngle(this.angle() + a);
-		return this;
-	},
-	limit: function(l)
-	{
-		var mag = this.mag();
-		if(mag > l)
-			this.setMag(l);
-		return this;
-	},
-	angleBetween: function(v)
-	{
-		return this.angle() - v.angle();
-	},
-	dot: function(v)
-	{
-		return this.x * v.x + this.y * v.y;
-	},
-	lerp: function(v, amt)
-	{
-		this.x += (v.x - this.x) * amt;
-		this.y += (v.y - this.y) * amt;
-		return this;
-	},
-	dist: function(v)
-	{
-		var dx = this.x - v.x;
-		var dy = this.y - v.y;
-		return Math.sqrt(dx * dx + dy * dy);
-	},
-	copy: function()
-	{
-		return new Vector(this.x, this.y);
-	},
-	random: function(){
-		this.set(1,1);
-		this.setAngle(Math.random() * Math.PI * 2);
+
 		return this;
 	}
-}
+
+	rotate(a: number) {
+		this.setAngle(this.angle() + a);
+
+		return this;
+	}
+
+	limit(l: number) {
+		var mag = this.mag();
+		if (mag > l)
+			this.setMag(l);
+
+		return this;
+	}
+
+	angleBetween(v: IVectorInterface) {
+		return this.angle() - v.angle();
+	}
+
+	dot(v: IVectorInterface) {
+		return this.x * v.x + this.y * v.y;
+	}
+
+	lerp(v: IVectorInterface, amt: number) {
+		this.x += (v.x - this.x) * amt;
+		this.y += (v.y - this.y) * amt;
+
+		return this;
+	}
+
+	dist(v: IVectorInterface) {
+		var dx = this.x - v.x;
+		var dy = this.y - v.y;
+
+		return Math.sqrt(dx * dx + dy * dy);
+	}
+
+	copy() {
+		return new Vector(this.x, this.y);
+	}
+
+	random() {
+		this.set(1, 1);
+		this.setAngle(Math.random() * Math.PI * 2);
+
+		return this;
+	}
+};
